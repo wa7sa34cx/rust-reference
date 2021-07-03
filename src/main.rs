e8b8381a7979ee38d68e7873c8eb2ac672b6f28f
Alex Black <wa7@sa34.cx>
2021-07-04 02:05:54 +0300

    trait implementation

--- src/main.rs
+++ src/main.rs
@@ -12,4 +12,36 @@ fn main() {
 
     let x = plus!(5.0, 7.2);
     println!("{}", x);
+
+    
+    // async { 
+    //     let p = 5;
+    //     let future = unsafe_example(&p);
+    //     future.await;
+    // }
+
+    let s = MyStruct::from("hello");
+    println!("{:#?}", s);
+
+    // let p = MyStruct { x: 77 };
+    let p: MyStruct = "heelo".into();
+    println!("{:#?}", p);
+
+    
+}
+
+async fn unsafe_example(x: &u32) {
+    println!{"{}", x};
+}
+
+#[derive(Debug)]
+struct MyStruct {
+    x: u32,
+}
+
+impl From<&str> for MyStruct {
+    #[inline]
+    fn from(_: &str) -> MyStruct {
+        MyStruct { x: 42 }
+    }
 }
