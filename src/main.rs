554c91063e2e4c6b86b26425be52b1bab9dd71d5
Alex Black <wa7@sa34.cx>
2021-07-03 15:17:58 +0300

    Simple macro

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,14 @@ macro_rules! example {
     () => { println!("Macro call in a macro!") };
 }
 
+macro_rules! plus {
+    ($t:tt, $v:tt) => { $t + $v; };
+}
+
 fn main() {
     example!();
+    println!();
+
+    let x = plus!(5.0, 7.2);
+    println!("{}", x);
 }
